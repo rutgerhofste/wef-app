6b381e3e9335fe9afbd0b75b0fef650d83e9a063
Rutger Hofste <rhofste.locals@WORLD-QEUEH81G4.internal.wri.org>
2019-01-22 18:01:49 +0100

    added description

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-df = pd.read_csv("wef_global_risk_v03.csv")
+df = pd.read_csv("wef_global_risk_v04.csv")
 
 
 def prepare_dataframe(df):
@@ -90,7 +90,7 @@ options = populate_dropdown(df_total)
 # Components
 
 
-title = html.H2('Global Risks')
+title = html.H2('World Economic Forum Global Risks')
 
 dropdown = dcc.Dropdown(id="dropdown",
                         options=options,
@@ -101,28 +101,64 @@ slider = dcc.RangeSlider(id='year-slider',
                          min=df_total.year.min(),
                          max=df_total.year.max(),
                          marks={str(year): str(year) for year in df['year'].unique()},
-                         value=[df_total.year.max(),df_total.year.max()])
+                         value=[df_total.year.max()-2,df_total.year.max()]
+                         )
+
 scatter = dcc.Graph(id="global_risk_scatter",
                     style={"margin-top": "50",
                            "margin-left": "50",
-                           "margin-right": "50"})
-title2 = html.H2('Global Risks (likelihood times impact) Ranked')
+                           "margin-right": "50",
+                           "height": 600})
 
 line = dcc.Graph(id="global_risk_line",
-                 style={"margin-top": "50",
+                 style={"margin-top": "200",
                         "margin-left": "50",
-                        "margin-right": "50"})
+                        "margin-right": "50",
+                        "height": 600})
+
+
+footer = dcc.Markdown("""
+For each of the 30* [global risks](https://www.weforum.org/reports/the-global-risks-report-2019), respondents
+were asked to assess (1) the
+likelihood of the risk occurring
+globally within the next 10 years,
+and (2) its negative impact for
+several countries or industries
+over the same timeframe. 
+
+The risks are categorized:
+1. Economic (Blue)  
+1. Societal (Red)  
+1. Environmental (Green)  
+1. Technological (Purple)  
+1. Geopolitical (Orange)  
+
+Source: [World Economic Forum.](https://www.weforum.org/reports/the-global-risks-report-2019)   
+Author: Rutger Hofste.  
+Infographics might contain errors.  
+Source Code: [GitHub](https://github.com/rutgerhofste/wef-app)  
+Download data (see WEF license): [GitHub](https://github.com/rutgerhofste/wef-app/blob/master/wef_global_risk_v04.csv)
+
+*50 risks in 2012 and 2013
+""")
+
+
+filter_component = html.Div([html.H6("Select Risks"),
+                             dropdown,
+                             html.H6("Select Years"),
+                             slider],
+                             style={"margin-top": "50",
+                                    "margin-left": "50",
+                                    "margin-right": "50"})
 
 
 
 app.layout = html.Div([
     title,
-    html.Div([dropdown,slider],style={"margin-top": "50",
-                           "margin-left": "50",
-                           "margin-right": "50"}),
+    filter_component,
     scatter,
-    title2,
-    line
+    line,
+    footer
 ])
 
 
@@ -141,6 +177,7 @@ def update_figure(slider_years,dropdown_values):
 
     for risk in risks:
         df_by_risk= df_filtered[df_filtered['risk_renamed'] == risk]
+
         category = df_by_risk["category"].iloc[0]
 
         traces.append(go.Scatter(
@@ -160,30 +197,30 @@ def update_figure(slider_years,dropdown_values):
         ))
 
     for dropdown_value in dropdown_values:
-        df_filtered = df_total[(df_total.year >= slider_years[0]) & (df_total.year <= slider_years[1])]
-        df_by_risk= df_filtered[df_filtered['risk_renamed'] == dropdown_value]
-        category = df_by_risk["category"].iloc[0]
-        traces.append(go.Scatter(
-            x=df_by_risk['x_norm'],
-            y=df_by_risk['y_norm'],
-            text= df_by_risk["text"],
-            mode='lines+markers',
-            opacity=1,
-            marker={
-                'size': 15,
-                'line': {'width': 0.5, 'color': 'black'},
-                'color': colors[category],
-                'symbol':'diamond'
-            },
-            name=risk,
-            showlegend=False
-        ))
+        df_by_risk = df_total[(df_total.year >= slider_years[0]) & (df_total.year <= slider_years[1]) & (df_total.risk_renamed==dropdown_value) ]
+        if df_by_risk.shape[0] >0 :
+            category = df_by_risk["category"].iloc[0]          
+            traces.append(go.Scatter(
+                x=df_by_risk['x_norm'],
+                y=df_by_risk['y_norm'],
+                text= df_by_risk["text"],
+                mode='lines+markers',
+                opacity=1,
+                marker={
+                    'size': 15,
+                    'line': {'width': 0.5, 'color': 'black'},
+                    'color': colors[category],
+                    'symbol':'diamond'
+                },
+                name=risk,
+                showlegend=False
+            ))
 
     return {
         'data': traces,
         'layout': go.Layout(
-            xaxis={'title': 'Normalized Likelihood','range': [-3, 3]},
-            yaxis={'title': 'Normalized Impact', 'range': [-3, 3]},
+            xaxis={'title': 'Normalized Likelihood','range': [-3, 3],'fixedrange': True},
+            yaxis={'title': 'Normalized Impact', 'range': [-3, 3],'fixedrange': True},
             margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
             hovermode='closest'
         )
@@ -228,32 +265,31 @@ def update_line_figure(slider_years,dropdown_values):
         df_filtered = df_total[(df_total.year >= slider_years[0]) & (df_total.year <= slider_years[1])]
         df_by_risk= df_filtered[df_filtered['risk_renamed'] == dropdown_value]
         category = df_by_risk["category"].iloc[0]
-        traces_lines.append(go.Scatter(
-            x=df_by_risk['year'],
-            y=df_by_risk['xy_rank'],
-            text= df_by_risk["text"],
-            mode='lines+markers',
-            opacity=1,
-            marker={
-                'size': 15,
-                'line': {'width': 0.5, 'color': 'black'},
-                'color': colors[category],
-                'symbol':'diamond'
-            },
-            showlegend=False)
-        )            
+        if category:
+            traces_lines.append(go.Scatter(
+                x=df_by_risk['year'],
+                y=df_by_risk['xy_rank'],
+                text= df_by_risk["text"],
+                mode='lines+markers',
+                opacity=1,
+                marker={
+                    'size': 15,
+                    'line': {'width': 0.5, 'color': 'black'},
+                    'color': colors[category],
+                    'symbol':'diamond'
+                },
+                showlegend=False)
+            )            
     
 
     return {"data":traces_lines,
             'layout': go.Layout(
-                xaxis={'title': 'year','range': [2012, 2019]},
-                yaxis={'title': 'Rank', 'range': [30,-1]},
+                xaxis={'title': 'year','range': [2012, 2019],'fixedrange': True},
+                yaxis={'title': 'Risks ranked by likelihood*impact', 'range': [30,-1],'fixedrange': True},
                 margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
                 hovermode='closest',
                 modebar={})
             }
-
-
 
 
 if __name__ == '__main__':
